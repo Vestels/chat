aa7201566e5edab1f472f4dec5ee7362272df459
Rokszin Roland <rokszinroland@gmail.com>
2024-12-16 09:39:06 +0100

    .env update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { DbConfig } from './config/db';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +10,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-  const PORT = DbConfig.port || 5000;
+  const PORT = parseInt(process.env.PORT) || 5000;
 
   await app.listen(PORT, () => {
     Logger.log(`App is running on PORT: ${PORT}`);
